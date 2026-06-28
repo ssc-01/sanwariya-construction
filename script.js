fcf34019b11b27774a5267dfc573038227fec185
sahilsutharr <146430919+sahilsutharr@users.noreply.github.com>
2026-06-28 08:05:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("Sanwariya Construction Website Ready");
+
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach(dropdown => {
+
+dropdown.addEventListener("click", () => {
+
+const content = dropdown.nextElementSibling;
+
+if(content.style.display === "block"){
+
+content.style.display = "none";
+
+}
+else{
+
+content.style.display = "block";
+
+}
+
+});
+
+});
